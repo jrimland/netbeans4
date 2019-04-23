d5b96276570eff6ce23b43583b009713455d0943
Jeff <jeff@Jeffs-MacBook-Pro.local>
2019-04-23 16:08:23 -0400

    edited this in vi

--- NetBeans4/src/App.java
+++ NetBeans4/src/App.java
@@ -15,6 +15,7 @@ public class App {
      */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+	System.out.println("Editing with vi - it sorta works - maybe");
     }
     
 }
