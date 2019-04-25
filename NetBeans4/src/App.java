cac76c15a1ee8ef3526430d8bccbbfef9c42e943
Jeff <jeff@Jeffs-MacBook-Pro.local>
2019-04-25 12:57:47 -0400

    added comment

--- NetBeans4/src/App.java
+++ NetBeans4/src/App.java
@@ -16,6 +16,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 	System.out.println("Editing with vi - it sorta works - maybe");
+	//a change
     }
     
 }
